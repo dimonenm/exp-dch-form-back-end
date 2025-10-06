091fb71e1821f6f3a39266ff4ae6b85c4045b93f
exp <arzuakov@yahoo.com>
2025-10-07 00:39:16 +0300

    h07.10.2025

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,35 +6,32 @@ import { hash } from 'argon2'
 
 @Injectable()
 export class UserService {
-	constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-	async createUser(userDto: CreateUserDto): Promise<User | null> {
+  async createUser(userDto: CreateUserDto): Promise<User | null> {
+    const isExist = await this.findUserByLogin(userDto.login);
 
-		const isExist = await this.findByLogin(userDto.login)
+    if (isExist) {
+      throw new ConflictException(
+        'Регистрация не удалась. Пользователь с таким email уже существует.',
+      );
+    }
 
-		if (isExist) {
-			throw new ConflictException('Регистрация не удалась. Пользователь с таким email уже существует.')
-		}
+    const newUser: Pick<User, 'login' | 'passwordHash'> = {
+      login: userDto.login,
+      passwordHash: userDto.password ? await hash(userDto.password) : '',
+    };
 
-		const newUser: Omit<User, "id" & "createdAt" & "updatedAt">  = {
-			login: userDto.login,
-			passwordHash: userDto.password ? await hash(userDto.password) : ''
-		}
+    return this.prisma.user.create({
+      data: newUser,
+    });
+  }
 
-		return this.prisma.user.create(
-			{
-				data: newUser
-			}
-		)
-	}
+  async findUserByLogin(login: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { login },
+    });
 
-	async findByLogin(login: string): Promise<User | null> {
-
-		const user = await this.prisma.user.findUnique({
-			where: { login }
-		})
-
-		return user
-
-	}
+    return user;
+  }
 }
