12f765471a5c4cf15cdec7abfeb546acf7f8b698
arzuakov@yahoo.com <arzuakov@yahoo.com>
2025-10-07 17:33:10 +0300

    w07.10.2025

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, ConflictException, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { CreateUserDto } from 'src/user/dto/createUser.dto'
+import { User } from 'generated/prisma'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+	constructor(private authService: AuthService) { }
+
+	@HttpCode(HttpStatus.OK)
+	@Post('create_user')
+	async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+		const user = await this.authService.createUser(createUserDto)
+		if (!user) {
+			throw new ConflictException(
+				'Регистрация не удалась.'
+			)
+		}
+		return user
+	}
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/createUser.dto'
+import { UserService } from 'src/user/user.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(private userService: UserService) { }
+
+	createUser(createUserDto: CreateUserDto){
+		return this.userService.createUser(createUserDto)
+	}
+
+}
